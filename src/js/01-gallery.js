b2560c98b95522b17d970fcbbb0309167452cb0f
grant956 <68757554+Psihodel666@users.noreply.github.com>
2022-07-04 14:48:47 +0300

    save

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,53 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgConteiner = document.querySelector('.gallery');
+
+const creatItem = (galleryItems) => {
+    return galleryItems.map(({preview, original, description}) =>{
+        return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+                </a>
+            </div>`
+    }).join('');
+};
+
+imgConteiner.insertAdjacentHTML('beforeend', creatItem(galleryItems));
+
+imgConteiner.addEventListener('click', openImg);
+
+function openImg(e){
+    e.preventDefault();
+    const galleryImg = e.target.classList.contains('gallery__image');
+    if(!galleryImg){
+        return
+    }
+    const eventImg = e.target.dataset.source;
+
+    let instance = basicLightbox.create(`
+    <img src="${eventImg}"  width="800" height="600">
+`)
+
+    instance.show()
+    imgConteiner.addEventListener('keydown', escapeFunction);
+    function escapeFunction(e){
+        if(e.code !== 'Escape'){
+            return;
+        }
+       
+           
+        basicLightbox.visible(false)
+    
+        
+        console.log(e.code)
+    }
+}
+
